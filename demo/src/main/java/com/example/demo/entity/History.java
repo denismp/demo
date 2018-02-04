c981eb6efc20623e32e1f3f2d391f8a3b87c0d60
Denis Putnam <denisputnam@verizon.net>
2018-02-04 14:47:34 -0500

    Added History and Author, and modified the other entities to extend History.

---
+++ demo/src/main/java/com/example/demo/entity/History.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author denisputnam
+ *
+ */
+@Component
+@MappedSuperclass
+public class History {
+	private String createdBy;
+	private Date createdDate;
+	private String updatedBy;
+	private Date updatedDate;
+	/**
+	 * @return the createdBy
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	/**
+	 * @param createdBy the createdBy to set
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	/**
+	 * @return the createdDate
+	 */
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	/**
+	 * @return the updatedBy
+	 */
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	/**
+	 * @param updatedBy the updatedBy to set
+	 */
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	/**
+	 * @return the updatedDate
+	 */
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+	/**
+	 * @param updatedDate the updatedDate to set
+	 */
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+}
