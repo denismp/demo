fde4053ca7dbb8e3c2252987af18f37b875374c1
Denis Putnam <denisputnam@verizon.net>
2019-01-30 15:21:12 -0500

    Changed the client port

--- demo/src/main/java/com/example/demo/configuration/MysqlConfiguration.java
+++ demo/src/main/java/com/example/demo/configuration/MysqlConfiguration.java
@@ -120,7 +120,7 @@ public class MysqlConfiguration {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http://localhost:4200");
+		config.addAllowedOrigin("http://localhost:4500");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
 		source.registerCorsConfiguration("/**", config);
